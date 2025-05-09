0164b036ad4070acabe6669034a73b91efec22c4
Nima Asaadi <ni4.asadi.b@gmail.com>
2025-05-09 17:48:39 +0330

    merge the configuration file

--- src/LaravelZoomServiceProvider.php
+++ src/LaravelZoomServiceProvider.php
@@ -9,6 +9,10 @@ class LaravelZoomServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        // Merge the configuration file
+        $this->mergeConfigFrom(__DIR__.'/../config/zoom.php', 'zoom');
+
+        // Register the ZoomService singleton
         $this->app->singleton('zoom', fn ($app): ZoomService => new ZoomService(
             config('zoom.base_url'),
             config('zoom.account_id'),
@@ -19,6 +23,7 @@ class LaravelZoomServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        // Publish the configuration file
         $this->publishes([
             __DIR__.'/../config/zoom.php' => config_path('zoom.php'),
         ]);
