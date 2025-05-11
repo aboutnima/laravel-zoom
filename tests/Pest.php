b4dff10cf85364e1dc3efb606c645268ca19304e
Nima Asaadi <ni4.asadi.b@gmail.com>
2025-05-11 12:11:57 +0330

    config test setup

---
+++ tests/Pest.php
@@ -0,0 +1,10 @@
+<?php
+
+use Aboutnima\LaravelZoom\Facades\Zoom;
+use Aboutnima\LaravelZoom\Services\ZoomService;
+
+uses(\Tests\TestCase::class)->in(__DIR__);
+
+it('does tests work', function () {
+    expect(true)->toBeTrue();
+});
